b856a9180487478710b81d3fc245e5bcb6560005
paulsterritt <psterritt5@gmail.com>
2024-05-01 11:52:14 -0400

    Added comments to explain code. Slight reformatting.

--- walkabilityApp/processor/Processor.py
+++ walkabilityApp/processor/Processor.py
@@ -2,27 +2,35 @@ from walkabilityApp.datamanagement.DataFetcher import DataFetcher
 class Processor:
     # Constructor
     def __init__(self, gps_coordinates):
-        self.data_fetcher = DataFetcher(gps_coordinates)
+        self.data_fetcher = DataFetcher(gps_coordinates) # create an instance of DataFetcher
+        self.dict_results = None
     # Instance Methods
+    '''
+    This is the main access point to be called in app.py or CommandLineUserInterface.py. Uses instance variable
+    data_fetcher to fetch data from googlemaps api for specified location and stores in a dictionary. Then calculates
+    and returns a walkability metric based on the fetched data.
+    '''
     def process_location(self):
         # Get data for specified loc w/ DataFetcher instance
-        dict_results = self.data_fetcher.perform_search()
+        self.dict_results = self.data_fetcher.perform_search()
         # compute walkability score
         score = self.calculate_walkability_score(dict_results=dict_results)
         # return interpreted score
         return self.interpret_score(walkability_score=score)
-
-    def calculate_walkability_score(self,dict_results):
+    '''
+    Helper method which calculates walkability score for an instance of Processor
+    '''
+    def calculate_walkability_score(self):
         # Walkability = sum(score x weight) for each category / sum(3* weight)
         # Weight of categories - high =3, medium=2, low=1:
         # Score of categories - great = 3, good = 2, low = 1, unacceptable = 0
         numerator = 0.0
         # High importance
-        numerator += self.get_category_score(key='grocery', dict_results=dict_results) * 3 # weight of groceries is 3
-        numerator += self.get_category_score(key='health_and_well_being', dict_results=dict_results) * 3
+        numerator += self.get_category_score(key='grocery') * 3 # weight of groceries is 3
+        numerator += self.get_category_score(key='health_and_well_being') * 3
         # Medium importance
-        numerator += self.get_category_score(key='eat_out', dict_results=dict_results) * 2
-        numerator += self.get_category_score(key='public_transit', dict_results=dict_results) * 2
+        numerator += self.get_category_score(key='eat_out') * 2
+        numerator += self.get_category_score(key='public_transit') * 2
         # Low weight = 1
         # As of now none could update categories or use a user defined
 
@@ -30,9 +38,12 @@ class Processor:
         denominator = 3*3*2 + 3*2*2 # currently we have 2 categories with high importance and 2 with medium importance
 
         return numerator/denominator*10
-
-    def get_category_score(self, key, dict_results):
-        length_value_set = len(dict_results[key])
+    '''
+    Helper method for calculate_walkability_score which assigns a 0-3 score for a given category based on number of
+    results obtained (e.g. 3 restaurants -> a score of 2) 
+    '''
+    def get_category_score(self, key):
+        length_value_set = len(self.dict_results[key])
         if(length_value_set == 0):
             return 0 #unaccetable
         elif(length_value_set < 2):
@@ -41,15 +52,18 @@ class Processor:
             return 2 #good
         else:
             return 3 # great
-
+    '''
+    Helper method which interpets the calculated x/10 walkability score and returns a user friendly string.
+    Chose to put this in Processor since it is interpreting data, however could argue that it belongs in ui tier.
+    '''
     def interpret_score(self, walkability_score):
         interpretation = 'The walkability score of the provided location is: ' + str(walkability_score) + ' out of 10 which is '
         if walkability_score < 2.5:
-            interpretation+="a very low score. This area is not walkable."
+            interpretation += "a very low score. This area is not walkable."
         elif walkability_score < 5:
-            interpretation+="a low score. This area is not very walkable."
+            interpretation += "a low score. This area is not very walkable."
         elif walkability_score < 7.5:
-            interpretation+="a good score! This area is pretty walkable!"
+            interpretation += "a good score! This area is pretty walkable!"
         else:
-            interpretation+="a great score! This area is very walkable!"
+            interpretation += "a great score! This area is very walkable!"
         return interpretation
--- walkabilityApp/ui/CommandLineUserInterface.py
+++ walkabilityApp/ui/CommandLineUserInterface.py
@@ -1,9 +1,12 @@
 from walkabilityApp.processor.Processor import Processor
 class CommandLineUserInterface:
     # instance methods:
+    '''
+    Main Access point for main. Prompts user for gps coordinates and returns the walkability result.
+    '''
     def start(self):
         stop = False
-        while (not stop):
+        while not stop:
             print("Enter gps coordinates: ")
             coordinates = input()
             try:
@@ -12,9 +15,13 @@ class CommandLineUserInterface:
                 print("Invalid input. Please enter gps coordinates.")
             print("Would you like to run another search (y/n)?")
             again = input().strip()
-            if (again[0] == 'n'):
+            if again[0] == 'n':
                 stop = True
-
+    '''
+    Helper method - Takes user input of gps coordinates and attempts to obtain a walkability metric by creating an 
+    instance of Processor & calling process_location(). 
+    Will throw an exception if the specified location is not valid.
+    '''
     def obtain_result(self, gps_coordinates):
         processor = Processor(gps_coordinates)
         return processor.process_location()
